951c51913e7d2fd86a242206ffa3df6ff32fe06a
MiaMio <kazanesama@vip.qq.com>
2017-06-01 21:46:46 +0800

    complete version 1.0

--- cinema.sql
+++ cinema.sql
@@ -54,7 +54,7 @@ CREATE TABLE `comments` (
   `comment_time` varchar(100) NOT NULL,
   `comment_score` int(11) NOT NULL,
   PRIMARY KEY (`comment_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -63,7 +63,7 @@ CREATE TABLE `comments` (
 
 LOCK TABLES `comments` WRITE;
 /*!40000 ALTER TABLE `comments` DISABLE KEYS */;
-INSERT INTO `comments` VALUES (1,'avatar','peter','good','2017-5-16',5),(2,'avatar','judy','suck','2017-5-16',5),(3,'avatar','alice','so so','2017-5-16',5),(4,'avatar','jack','yes, this is a suck film','2017-05-16',0),(5,'Chun Jiao jiu Zhi Ming','jack','this is a very moving film!','2017-05-21',5),(6,'Chun Jiao jiu Zhi Ming','jack','this is a very moving film!','2017-05-21',5),(7,'Chun Jiao jiu Zhi Ming','jack','This is a very moving film','2017-05-21',5);
+INSERT INTO `comments` VALUES (1,'avatar','peter','good','2017-5-16',5),(2,'avatar','judy','suck','2017-5-16',5),(3,'avatar','alice','so so','2017-5-16',5),(4,'avatar','jack','yes, this is a suck film','2017-05-16',0),(5,'Chun Jiao jiu Zhi Ming','jack','this is a very moving film!','2017-05-21',5),(6,'Chun Jiao jiu Zhi Ming','jack','this is a very moving film!','2017-05-21',5),(7,'Chun Jiao jiu Zhi Ming','jack','This is a very moving film','2017-05-21',5),(8,'Chun Jiao jiu Zhi Ming','alice1','This is very bad','2017-05-23',1),(9,'Chun Jiao jiu Zhi Ming','cabbage','This is trash','2017-06-01',5);
 /*!40000 ALTER TABLE `comments` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -78,8 +78,9 @@ CREATE TABLE `customer` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `customer_account` varchar(20) NOT NULL,
   `customer_pwd` varchar(20) NOT NULL,
+  `customer_balance` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -88,7 +89,7 @@ CREATE TABLE `customer` (
 
 LOCK TABLES `customer` WRITE;
 /*!40000 ALTER TABLE `customer` DISABLE KEYS */;
-INSERT INTO `customer` VALUES (1,'jack','jack123'),(2,'alice','alice123'),(3,'jack','');
+INSERT INTO `customer` VALUES (1,'jack','jack',1800),(2,'alice','alice123',0),(4,'administrator','administrator',0),(5,'alice1','alice123',0),(6,'cabbage','12345678',101);
 /*!40000 ALTER TABLE `customer` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -104,7 +105,7 @@ CREATE TABLE `film` (
   `film_name` varchar(100) NOT NULL,
   `film_abstract` varchar(1000) NOT NULL,
   PRIMARY KEY (`film_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -113,7 +114,7 @@ CREATE TABLE `film` (
 
 LOCK TABLES `film` WRITE;
 /*!40000 ALTER TABLE `film` DISABLE KEYS */;
-INSERT INTO `film` VALUES (1,'Chun Jiao jiu Zhi Ming','A couple who have been together for several years try to \r\n decide if they should stay together.'),(2,'Guardians of the Galaxy','Set to the backdrop of Awesome Mixtape #2, \'Guardians of the Galaxy \r\n Vol. 2\' continues the team\'s adventures as they unravel the mystery of \r\n Peter Quill\'s true parentage.'),(3,'Power Rangers','A group of high-school students, who are infused with unique superpowers, \r\n harness their abilities in order to save the world.'),(4,'Dangal','Former wrestler Mahavir Singh Phogat and his two wrestler daughters struggle \r\n towards glory at the Commonwealth Games in the face of societal oppression.');
+INSERT INTO `film` VALUES (1,'Chun Jiao jiu Zhi Ming','A couple who have been together for several years try to \r\n decide if they should stay together.'),(2,'Guardians of the Galaxy','Set to the backdrop of Awesome Mixtape #2, \'Guardians of the Galaxy \r\n Vol. 2\' continues the team\'s adventures as they unravel the mystery of \r\n Peter Quill\'s true parentage.'),(11,'Dangal','good');
 /*!40000 ALTER TABLE `film` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -132,7 +133,7 @@ CREATE TABLE `ticket` (
   `ticket_time` varchar(20) NOT NULL,
   `ticket_cinema` varchar(50) NOT NULL,
   PRIMARY KEY (`ticket_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -141,7 +142,7 @@ CREATE TABLE `ticket` (
 
 LOCK TABLES `ticket` WRITE;
 /*!40000 ALTER TABLE `ticket` DISABLE KEYS */;
-INSERT INTO `ticket` VALUES (1,'avatar',50,'jack','2017-5-15','yonghua'),(2,'avatar',50,'jack','2017-5-15','yonghua'),(3,'Chun Jiao jiu Zhi Ming',50,'jack','05-24 11:45','UA Cinema'),(4,'Chun Jiao jiu Zhi Ming',50,'jack','05-24 11:45','UA Cinema');
+INSERT INTO `ticket` VALUES (1,'avatar',50,'jack','2017-5-15','yonghua'),(2,'avatar',50,'jack','2017-5-15','yonghua'),(3,'Chun Jiao jiu Zhi Ming',50,'jack','05-24 11:45','UA Cinema'),(4,'Chun Jiao jiu Zhi Ming',50,'jack','05-24 11:45','UA Cinema'),(5,'Chun Jiao jiu Zhi Ming',50,'alice1','05-24 13:45','UA Cinema'),(6,'Chun Jiao jiu Zhi Ming',50,'administrator','05-24 11:00','SFC Cinema'),(7,'Chun Jiao jiu Zhi Ming',50,'administrator','05-29 11:00','SFC Cinema'),(8,'Chun Jiao jiu Zhi Ming',50,'administrator','05-29 11:00','SFC Cinema');
 /*!40000 ALTER TABLE `ticket` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -160,7 +161,7 @@ CREATE TABLE `timetable` (
   `cinema` varchar(30) NOT NULL,
   `time_film_price` int(11) NOT NULL,
   PRIMARY KEY (`time_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -169,7 +170,7 @@ CREATE TABLE `timetable` (
 
 LOCK TABLES `timetable` WRITE;
 /*!40000 ALTER TABLE `timetable` DISABLE KEYS */;
-INSERT INTO `timetable` VALUES (1,'2017-5-15','avatar',49,'yonghua',50),(2,'05-24 11:35','Dangal',50,'SFC Cinema',50),(3,'05-24 14:10','Dangal',50,'SFC Cinema',50),(4,'05-24 16:45','Dangal',50,'SFC Cinema',50),(5,'05-24 19:20','Dangal',50,'SFC Cinema',50),(6,'05-24 22:00','Dangal',50,'SFC Cinema',50),(7,'05-24 11:35','Dangal',50,'UA Cinema',50),(8,'05-24 13:00','Dangal',50,'UA Cinema',50),(9,'05-24 14:10','Dangal',50,'UA Cinema',50),(10,'05-24 15:35','Dangal',50,'UA Cinema',50),(11,'05-24	16:45','Dangal',50,'UA Cinema',50),(12,'05-24 11:45','Guardians of the Galaxy',50,'SFC Cinema',50),(13,'05-24 14:20','Guardians of the Galaxy',50,'SFC Cinema',50),(14,'05-24 16:55','Guardians of the Galaxy',50,'SFC Cinema',50),(15,'05-24 19:30','Guardians of the Galaxy',50,'SFC Cinema',50),(16,'05-24 22:05','Guardians of the Galaxy',50,'SFC Cinema',50),(17,'05-24 16:30','Guardians of the Galaxy',50,'UA Cinema',50),(18,'05-24 19:00','Guardians of the Galaxy',50,'UA Cinema',50),(19,'05-24 21:30','Guardians of the Galaxy',50,'UA Cinema',50),(20,'05-24 11:45','Power Rangers',50,'UA Cinema',50),(21,'05-24 13:45','Power Rangers',50,'UA Cinema',50),(22,'05-24 14:45','Power Rangers',50,'UA Cinema',50),(23,'05-24 15:45','Power Rangers',50,'UA Cinema',50),(24,'05-24 11:00','Power Rangers',50,'SFC Cinema',50),(25,'05-24 13:00','Power Rangers',50,'SFC Cinema',50),(26,'05-24 14:00','Power Rangers',50,'SFC Cinema',50),(27,'05-24 16:00','Power Rangers',50,'SFC Cinema',50),(28,'05-24 11:45','Chun Jiao Jiu Zhi Ming',49,'UA Cinema',50),(29,'05-24 13:45','Chun Jiao Jiu Zhi Ming',50,'UA Cinema',50),(30,'05-24 14:45','Chun Jiao Jiu Zhi Ming',50,'UA Cinema',50),(31,'05-24 15:45','Chun Jiao Jiu Zhi Ming',50,'UA Cinema',50),(32,'05-24 11:00','Chun Jiao Jiu Zhi Ming',50,'SFC Cinema',50),(33,'05-24 13:00','Chun Jiao Jiu Zhi Ming',50,'SFC Cinema',50),(34,'05-24 14:00','Chun Jiao Jiu Zhi Ming',50,'SFC Cinema',50),(35,'05-24 16:00','Chun Jiao Jiu Zhi Ming',50,'SFC Cinema',50);
+INSERT INTO `timetable` VALUES (1,'2017-5-15','avatar',49,'yonghua',50),(2,'05-24 11:35','Dangal',50,'SFC Cinema',50),(3,'05-24 14:10','Dangal',50,'SFC Cinema',50),(4,'05-24 16:45','Dangal',50,'SFC Cinema',50),(5,'05-24 19:20','Dangal',50,'SFC Cinema',50),(6,'05-24 22:00','Dangal',50,'SFC Cinema',50),(7,'05-24 11:35','Dangal',50,'UA Cinema',50),(8,'05-24 13:00','Dangal',50,'UA Cinema',50),(9,'05-24 14:10','Dangal',50,'UA Cinema',50),(10,'05-24 15:35','Dangal',50,'UA Cinema',50),(11,'05-24	16:45','Dangal',50,'UA Cinema',50),(14,'05-24 16:55','Guardians of the Galaxy',50,'SFC Cinema',50),(15,'05-24 19:30','Guardians of the Galaxy',50,'SFC Cinema',50),(17,'05-24 16:30','Guardians of the Galaxy',50,'UA Cinema',50),(20,'05-24 11:45','Power Rangers',50,'UA Cinema',50),(21,'05-24 13:45','Power Rangers',50,'UA Cinema',50),(22,'05-24 14:45','Power Rangers',50,'UA Cinema',50),(23,'05-24 15:45','Power Rangers',50,'UA Cinema',50),(24,'05-24 11:00','Power Rangers',50,'SFC Cinema',50),(25,'05-24 13:00','Power Rangers',50,'SFC Cinema',50),(26,'05-24 14:00','Power Rangers',50,'SFC Cinema',50),(27,'05-24 16:00','Power Rangers',50,'SFC Cinema',50),(29,'05-24 13:45','Chun Jiao Jiu Zhi Ming',49,'UA Cinema',50),(32,'05-24 11:00','Chun Jiao Jiu Zhi Ming',49,'SFC Cinema',50),(37,'05-29 11:00','Chun Jiao jiu Zhi Ming',48,'SFC Cinema',50),(39,'06-05 12:00','Chun Jiao jiu Zhi Ming',50,'SFC Cinema',50),(40,'06-10 12:00','Chun Jiao jiu Zhi Ming',50,'SFC Cinema',50),(41,'06-07','Guardians of the Galaxy',50,'SFC Cinema',50),(43,'06-08','Guardians of the Galaxy',2,'UA Cinema',100);
 /*!40000 ALTER TABLE `timetable` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -182,4 +183,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-05-21 21:02:33
+-- Dump completed on 2017-06-01 21:45:52
